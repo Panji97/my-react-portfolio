5ca91eb0ff9251be7a46c85eb7a257886cb60827
Panji97 <76486961+Panji97@users.noreply.github.com>
2022-08-13 10:12:30 +0700

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header__container">
         <h4>Hello I'm</h4>
         <h1>Panji Kurniawan</h1>
-        <h4 className="text-align">Frontend Developer</h4>
+        <h4 className="text-align">Backend Developer</h4>
         <CTA />
         <div className="container__wrapper">
           <HeaderSocials />
